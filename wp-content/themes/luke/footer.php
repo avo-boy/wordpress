30edcee5b212ad2d1c32579e4f3a99a7fcab97cf
avo-boy <lukevrensburg@gmail.com>
2018-08-27 09:25:33 +0200

    theme-add

---
+++ wp-content/themes/luke/footer.php
@@ -0,0 +1,34 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<div class="footercol30" id="left">
+
+				</div>
+				<div class="footercol40">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo_black_300px.png" alt="">
+				</div>
+				<div class="footercol30" id="right">
+
+				</div>
+
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
